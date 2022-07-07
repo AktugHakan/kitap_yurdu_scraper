e666ac41d2c683ea610b43063b9daed55f845df3
Ahmet Hakan Aktuğ <ahmethakanaktug@gmail.com>
2022-07-07 18:16:48 +0300

    started to implement a function that returns best seller list

--- scraper.rb
+++ scraper.rb
@@ -3,10 +3,19 @@ require 'nokogiri'.freeze
 
 url = 'https://www.kitapyurdu.com/cok-satan-kitaplar/haftalik/1.html'
 
-def get_dom(url)
-    raw_html = HTTParty.get(url)
-    binding.irb # DEBUG ONLY
-    Nokogiri.HTML5(raw_html)
-end
+module Scrapper
+    def get_dom(url)
+        raw_html = HTTParty.get(url)
+        binding.irb # DEBUG ONLY
+        return Nokogiri.HTML5(raw_html)
+    end
 
-dom = get_dom(url)
+    def best_seller_list(dom)
+        raise TypeError, 'Expected a parsed HTML file.' unless dom.respond_to? 'css'
+
+        product_cr_list = dom.css('div.product-cr')
+        product_cr_list.each do |product_cr|
+            #TODO
+        end
+    end
+end
