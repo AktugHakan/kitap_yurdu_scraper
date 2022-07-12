ae533c551a0aebfba8e4b37699ad344f05ddfeea
Ahmet Hakan Aktuğ <ahmethakanaktug@gmail.com>
2022-07-12 09:51:31 +0300

    Scrapper completed

--- scraper.rb
+++ scraper.rb
@@ -1,8 +1,8 @@
 require 'httparty'.freeze
 require 'nokogiri'.freeze
+require 'product.rb'.freeze
 
-url = 'https://www.kitapyurdu.com/cok-satan-kitaplar/haftalik/1.html'
-
+# Set of tools to GET and parse a website
 module Scrapper
     def get_dom(url)
         raw_html = HTTParty.get(url)
@@ -10,12 +10,29 @@ module Scrapper
         return Nokogiri.HTML5(raw_html)
     end
 
-    def best_seller_list(dom)
-        raise TypeError, 'Expected a parsed HTML file.' unless dom.respond_to? 'css'
+    def product_cr_renderer(product_cr)
+        raise TypeError, 'Expected a parsed HTML fragment/file.' unless dom.respond_to? 'css'
 
+        rank = product_cr.css('div.bestseller-rank > div').text.to_i
+        title = product_cr.css('div.name-ellipsis').text
+        author = product_cr.css('div.author > span').text
+        publisher = product_cr.css('div.publisher').text
+        price = product_cr.css('div.price-new > span.value').text.to_f
+        binding.irb
+
+        return Book.new title, author, publisher, price, rank
+    end
+
+    def best_seller_list(dom)
+        raise TypeError, 'Expected a parsed HTML fragment/file.' unless dom.respond_to? 'css'
+
+        books = []
         product_cr_list = dom.css('div.product-cr')
         product_cr_list.each do |product_cr|
-            #TODO
+            book = product_cr_renderer(product_cr)
+            books.append(book)
         end
+
+        return books
     end
 end
